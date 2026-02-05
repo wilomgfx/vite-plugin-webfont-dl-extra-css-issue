ec5c3d5a18eccacfc778c627b75f01059fcc012f
William Cantin <wzc01@acuitysso.com>
2026-02-05 16:19:23 -0500

    Remove unneeded packages

--- src/css.ts
+++ src/css.ts
@@ -1,6 +1,4 @@
-// CSS needed for devextreme components.
-// As documented here: https://js.devexpress.com/Documentation/21_1/Guide/React_Components/Add_DevExtreme_to_a_React_Application/
-import "devextreme/dist/css/dx.material.blue.light.css";
+import "./css.css";
 
 // We import fonts in the build to be able to use the fonts offline.
 import "@fontsource/roboto/300.css";
